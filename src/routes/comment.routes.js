c990d2238201fe89ce48b23e0fa2ccbd7eaba6a5
Sangita Halder <sangitah652l@gmail.com>
2026-03-23 15:23:59 +0530

    fix comment routes to include 'c' prefix for update and delete operations

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -6,7 +6,7 @@ const router= Router()
 
 
 router.route("/add-comment/:videoId").post(verifyJwt,addComment)
-router.route("/update-comment/:commentId").patch(verifyJwt,updateComment)
-router.route("/delete-comment/:commentId").delete(verifyJwt,deleteComment)
+router.route("/update-comment/c/:commentId").patch(verifyJwt,updateComment)
+router.route("/delete-comment/c/:commentId").delete(verifyJwt,deleteComment)
 router.route("/video-comments/:videoId").get(verifyJwt,getVideoComments)
 export default router
