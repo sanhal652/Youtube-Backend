203b0edb78fb085ae6f8658675ad93656f21e607
sanhal652 <sangitah652@gmail.com>
2026-08-20 10:17:53 +0530

    Delete frontend/frontend directory

--- frontend/frontend/src/axiosFiles/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api= axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:8000/api/v1",
-    withCredentials:true
-})
-
-export default api
--- frontend/frontend/src/components/container/Container.jsx
+++
@@ -1,9 +0,0 @@
-function Container({children})     //accepts property as children and returns a div with some styling and renders the children inside it
-{
-    return(
-        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            {children}
-        </div>
-    )
-}
-export default Container;
